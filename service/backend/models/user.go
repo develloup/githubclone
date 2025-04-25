c41f5520f1ac7697e2e1566875cf807178fa9f7f
Develloup <develloup@gmail.com>
2025-04-25 16:34:11 +0200

    feat: start to model user and connections

--- service/backend/models/user.go
+++ service/backend/models/user.go
@@ -3,9 +3,20 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-	ID    uint   `gorm:"primaryKey"`
-	Name  string `gorm:"size:100"`
-	Email string `gorm:"unique"`
+	ID          uint         `gorm:"primaryKey"`
+	Username    string       `gorm:"not null"`
+	Email       string       `gorm:"unique;not null"`
+	Connections []Connection `gorm:"many2many:user_connections;"`
+}
+
+type Connection struct {
+	ID             uint   `gorm:"primaryKey"`
+	URL            string // Mandatory for GHES instance, not necessary for github, gitlab
+	ClientID       string `gorm:"not null"`
+	ClientSecret   string `gorm:"not null"`
+	SessionStarted bool
+	AccessToken    string
+	Users          []User `gorm:"many2many:user_connections;"`
 }
 
 func AutoMigrate(db *gorm.DB) {
