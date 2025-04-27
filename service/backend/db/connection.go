aa841f79eb6d7267cde987baf87fc1165cdc8501
Develloup <develloup@gmail.com>
2025-04-27 16:18:52 +0200

    feat: add also enumerations in the AutoMigrate routine

--- service/backend/db/connection.go
+++ service/backend/db/connection.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"githubclone-backend/models"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -17,12 +18,52 @@ func InitDB() {
 	}
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", host, user, password, dbname)
+	name := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port)
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
 	}
+}
+
+func AutoMigrate() error {
+	enumDefinitions := []struct {
+		TypeName  string
+		CreateSQL string
+	}{
+		{"user_type", `CREATE TYPE user_type AS ENUM ('admin', 'user')`},
+		{"connection_type", `CREATE TYPE connection_type AS ENUM ('github', 'gitlab', 'ghes')`},
+		{"permission_type", `CREATE TYPE permission_type AS ENUM ('CreateUser', 'DeleteUser', 'EditUser')`},
+	}
+
+	for _, enum := range enumDefinitions {
+		// Delete the type if it already exists
+		dropSQL := `DROP TYPE IF EXISTS ` + enum.TypeName + ` CASCADE`
+		if err := DB.Exec(dropSQL).Error; err != nil {
+			return err
+		}
+
+		// Create the type
+		if err := DB.Exec(enum.CreateSQL).Error; err != nil {
+			return err
+		}
+	}
+
+	models := []interface{}{
+		&models.User{},
+		&models.Permission{},
+		&models.Connection{},
+		&models.UserConnection{},
+		// Add further models here
+	}
+
+	for _, m := range models {
+		if err := DB.AutoMigrate(m); err != nil {
+			return err
+		}
+	}
+	return nil
 }
