b68acba51205ab2a70cdbfb298b5fe4291d6ae80
Develloup <develloup@gmail.com>
2025-04-28 17:53:06 +0200

    chore: skeleton for user administration

---
+++ service/backend/api/user.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Login(c *gin.Context, db *gorm.DB) {
+
+}
+
+func Logout(c *gin.Context) {
+
+}
+
+func CreateUser(c *gin.Context, db *gorm.DB) {
+
+}
+
+func UpdateUser(c *gin.Context, db *gorm.DB) {
+
+}
+
+func DeleteUser(c *gin.Context, db *gorm.DB) {
+
+}
