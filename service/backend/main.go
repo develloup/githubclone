d0ee3150657459e4a5d6258addf2394d92a4f21b
Develloup <develloup@gmail.com>
2025-04-28 17:52:51 +0200

    feat: set logfile for logger and set middleware explicitly

--- service/backend/main.go
+++ service/backend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"githubclone-backend/db"
 	"githubclone-backend/models"
+	"io"
+	"log"
 	"net/http"
 	"os"
 
@@ -43,10 +45,27 @@ var gitlabOAuth = &oauth2.Config{
 }
 
 func main() {
+
+	logFile, err := os.OpenFile("githubclone.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Fehler beim Öffnen/Erstellen der Log-Datei: %v", err)
+	}
+	defer logFile.Close()
+
+	// Multi-Writer für stdout, stderr und Datei
+	multiWriter := io.MultiWriter(os.Stdout, os.Stderr, logFile)
+
+	// Setze die Ausgabe des log-Pakets
+	log.SetOutput(multiWriter)
 	db.InitDB()
 	db.AutoMigrate()
 
-	r := gin.Default()
+	r := gin.New()        // Create an engine with a middleware
+	r.Use(gin.Logger())   // Add a logger
+	r.Use(gin.Recovery()) // Add recovery
+	if err := r.SetTrustedProxies(nil); err != nil {
+		log.Printf("Cannot set trusted proxies: %v", err)
+	}
 
 	r.GET("/login/:provider", func(c *gin.Context) {
 		provider := c.Param("provider")
