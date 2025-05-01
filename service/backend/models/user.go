51ae31d129dd5c45d098e89614ce57e250f9a6d5
Develloup <develloup@gmail.com>
2025-05-01 21:15:41 +0200

    feat: update of user and connection

--- service/backend/models/user.go
+++ service/backend/models/user.go
@@ -6,28 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserInput struct {
-	Username       string       `json:"username" binding:"required"`
-	Email          string       `json:"email" binding:"required,email"`
-	PasswordExpiry time.Time    `json:"passwordExpiry"`
-	Description    string       `json:"description"`
-	Deactivated    bool         `json:"deactivated"`
-	PasswordSet    bool         `json:"passwordSet"`
-	UserType       string       `json:"type" binding:"required"`
-	Permissions    []Permission `json:"permissions"`
-}
-
 type User struct {
 	gorm.Model
-	Username       string    `gorm:"unique;not null"`
-	Email          string    `gorm:"not null"`
-	PasswordHash   string    `gorm:"not null"`
-	AccountCreated time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	AccountUpdated time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	PasswordExpiry time.Time `gorm:"not null"`
-	Description    string
-	Deactivated    bool
-	PasswordSet    bool         `gorm:"not null"`                   // Boolean value to force to set a new password
+	Username       string       `gorm:"unique;not null"`
+	Email          string       `gorm:"default:''"`
+	PasswordHash   string       `gorm:"not null;default:''"`
+	CreatedAt      time.Time    `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time    `gorm:"default:CURRENT_TIMESTAMP"`
+	PasswordExpiry time.Time    `gorm:"not null"`
+	Description    string       `gorm:"default:''"`
+	Deactivated    bool         `gorm:"not null;default:false"`
+	Deletable      bool         `gorm:"not null;default:true"`
+	PasswordSet    bool         `gorm:"not null;default:true"`      // Boolean value to force to set a new password
 	UserType       string       `gorm:"type:user_type;not null"`    // Enum for a user type
 	Permissions    []Permission `gorm:"many2many:user_permissions"` // Many-to-many relation to permissions
 	Connections    []Connection `gorm:"many2many:user_connections"` // Many-to-many relation to connections
@@ -46,16 +36,19 @@ type UserPermission struct {
 
 type Connection struct {
 	gorm.Model
-	Type           string  `gorm:"type:connection_type;not null"`
-	URL            *string // Optional, therefore defined as a pointer, mandatory only for GHES
-	ClientID       string  `gorm:"not null"`
-	ClientSecret   string  `gorm:"not null"`
-	SessionStarted time.Time
-	AccessToken    string
+	ConnectionName string    `gorm:"unique;not null"`
+	Type           string    `gorm:"type:connection_type;not null"`
+	URL            *string   // Optional, therefore defined as a pointer, mandatory only for GHES
+	ClientID       string    `gorm:"not null"`
+	ClientSecret   string    `gorm:"not null"`
+	CreatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	Description    string    `gorm:"default:''"`
+	Deactivated    bool      `gorm:"not null"`
 }
 
 type UserConnection struct {
 	UserID       uint      `gorm:"primaryKey"`
 	ConnectionID uint      `gorm:"primaryKey"`
-	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	CreatedAt    time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
 }
