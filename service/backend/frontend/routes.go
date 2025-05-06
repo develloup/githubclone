e36cf9420f29ec6521685bfa32c7c00941126dc8
Develloup <develloup@web.de>
2025-05-06 20:47:09 +0200

    fix: explicit routing to login.html

--- service/backend/frontend/routes.go
+++ service/backend/frontend/routes.go
@@ -11,12 +11,16 @@ func Routes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.File("./static/index.html")
 	})
+	r.GET("/login", func(c *gin.Context) {
+		c.File("./static/login.html")
+	})
+
 	r.NoRoute(func(c *gin.Context) {
 		path := "./static" + c.Request.URL.Path
 		if _, err := http.Dir("./static").Open(c.Request.URL.Path); err == nil {
-			c.File(path) // deliver static file
+			c.File(path) // Datei existiert
 		} else {
-			c.File("./static/index.html") // Next.js takes care of finding illegal pages
+			c.File("./static/index.html") // Next.js übernimmt Routing
 		}
 	})
 }
