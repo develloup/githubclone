3074a5b0228f8aa22507f682f03933d563d6f750
Develloup <develloup@web.de>
2025-05-21 16:25:10 +0200

    chore: types used in the frontend are located here, not in the component itself
    This makes it easier to handle one type between components.

---
+++ service/frontend/src/types/types.ts
@@ -0,0 +1,23 @@
+
+type User = {
+  username: string;
+  email: string;
+} | null;
+
+type OAuthUser = {
+  data: {
+    viewer: {
+      login: string;       // User name
+      name: string;        // Full name
+      email: string;       // Public email (if available)
+      bio: string;         // Description / Biography
+      avatarUrl: string;   // Avatar picture URL
+      createdAt: string;   // Account creation date
+      company?: string;    // Company or Organization (optional)
+      location: string;    // User location
+      websiteUrl?: string; // Personal website (optional)
+    };
+  };
+};
+
+export type {User, OAuthUser};
